0dafd0ca215513f7833a946a81948069dc8ade97
rocio <rocioma324@gmail.com>
2022-05-09 11:39:54 -0500

    Primer Cambio

---
+++ src/componentes/Lista/Lista.js
@@ -0,0 +1,16 @@
+const List = () =>{ 
+    return(
+        <div>
+            <h2>Things I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>    
+            </ul>
+        </div>        
+
+    )
+}
+
+export default List;
